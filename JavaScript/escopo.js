5af5ba96c4788c55c55aab2f06d7840976cbb29f
Arthur Dias Gomes Gerhardt <66851119+arthurgerhardt@users.noreply.github.com>
2024-10-11 11:19:33 -0300

    Add files via upload

---
+++ JavaScript/escopo.js
@@ -0,0 +1,9 @@
+let estudante;
+if (1 > 0) {
+    estudante = 'Arthur';
+    console.log(estudante);
+    console.log("O nome do estudante é " + estudante);
+}
+
+console.log(estudante);
+console.log("O nome do estudante é " + estudante);
