80d06ba6c9a4b220afe7b008781192ef4055b4f0
Arthur <renewedjesus@gmail.com>
2024-10-14 16:45:00 -0300

    Mais conteúdo

---
+++ Curso_Alura/JavaScript/tipo-string.js
@@ -0,0 +1,5 @@
+const estudante = "Arthur";
+const docente = "Raphael";
+const cumprimento = "Nosso lema é ' estudar bastante '";
+console.log(estudante, docente, cumprimento);
+console.log('O estudante chama ' + estudante);
