68156fd0bb8ca98e09e0b14db9f4d52750e3c3b2
Arthur <renewedjesus@gmail.com>
2024-10-15 11:33:42 -0300

    Mais código!

--- Curso_Alura/JavaScript/tipo-string.js
+++ Curso_Alura/JavaScript/tipo-string.js
@@ -1,5 +1,11 @@
 const estudante = "Arthur";
 const docente = "Raphael";
 const cumprimento = "Nosso lema é ' estudar bastante '";
-console.log(estudante, docente, cumprimento);
-console.log('O estudante chama ' + estudante);
+const citacao = `Raphael diz que o nosso lema é 'estudar.'`;
+console.log(cumprimento);
+console.log(citacao);
+console.log(`O estudante chama ${estudante} e o docente chama ${docente}.`);
+
+// Template strings
+const senha = 'SenhaSegura123' + estudante.toUpperCase();
+console.log(senha);
