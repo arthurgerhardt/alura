37762445eec7955101b62117d79eb0be8d09c89d
arthurgerhardt <renewedjesus@gmail.com>
2025-03-06 17:24:30 -0300

    Aulas de JavaScript

---
+++ Cursos/JavaScript/listas1.js
@@ -0,0 +1,7 @@
+const alunos = ['João', 'Juliana', 'Caio', 'Ana']
+const medias = [10, 8, 7.5, 9]
+
+// console.log(alunos[0], medias[0])
+// console.log(alunos[1], medias[1])
+const lista = [alunos, medias]
+console.log(lista)
