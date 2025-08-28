9854581f7213274a18a11da70a3b4bd6d4dc4cd4
“arthurgerhardt” <renewedjesus@gmail.com>
2025-08-28 11:27:45 -0300

    Avançando no curso de Java.

--- Java/Aulas/ExemploRepeticao.java
+++ Java/Aulas/ExemploRepeticao.java
@@ -1,14 +1,35 @@
+import java.util.Scanner;
+
 public class ExemploRepeticao {
     public static void main(String[] args) {
         String[] vendedores = {"Carlos", "Mariana", "João", "Fernanda"};
         double[] vendas = {4000.0, 8000.0, 12000.0, 5000.0};
+
+        for (int i = 0; i < vendedores.length; i++) {
+            System.out.printf("%s - comissão %.2f\n", vendedores[i], calcularComissao(vendas[i]));
+        }
+
+        System.out.println("Teste com o while");
+        int j = 0;
+        boolean imprimeOutro = true;
+        Scanner leitura = new Scanner(System.in);
+
+        while(imprimeOutro) {
+            System.out.printf("%s - comissão %.2f\n", vendedores[j], calcularComissao(vendas[j]));
+            j++;
+            System.out.println("Imprimir outro? (true/false)");
+            imprimeOutro = leitura.nextBoolean();
+        }
     }
 
     public static double calcularComissao(double totalVendas) {
-        if (totalVendas <= 10000.0) {
+        if (totalVendas <= 5000.0) {
             return totalVendas * 0.03;
+        } else if (totalVendas <= 10000.0) {
+            return totalVendas * 0.05;
         }
-        // You may want to handle cases where totalVendas > 10000.0
-        return totalVendas * 0.05;
+        else {
+            return totalVendas * 0.07;
+        }
     }
 }
---
+++ Java/Aulas/SubirDegrau.java
@@ -0,0 +1,10 @@
+public class SubirDegrau {
+    public static void main(String[] args) {
+        int degraus = 5;
+        int degrausPorVez = 1;
+        int totalSubidas = degraus / degrausPorVez;
+
+        System.out.println("Você subirá " + totalSubidas + " vezes.");
+    }
+}
+
