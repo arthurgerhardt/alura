d6332f4f41731c786bf2598d4a01ca0561f150dc
“arthurgerhardt” <renewedjesus@gmail.com>
2025-09-01 16:06:40 -0300

    Revisando conteúdo de Java, do início.

--- Java/Projetos/meuprimeiroprograma/src/App.java
+++ Java/Projetos/meuprimeiroprograma/src/App.java
@@ -1,5 +1,10 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        System.out.println("Esse é o Screen Match!");
+        System.out.println("Filme: Top Gun: Maverick");
+        
+        int anoDeLancamento = 2022;
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
+        
     }
 }
