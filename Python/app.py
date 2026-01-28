69874540258cd50da5a50d27ff1c0a1053c81a82
“arthurgerhardt” <renewedjesus@gmail.com>
2026-01-28 13:59:51 -0300

    Voltando a praticar Python.

---
+++ Python/app.py
@@ -0,0 +1,6 @@
+# Aula de Python da Alura
+# Primeiros passos em Python, revisando conceitos básicos da linguagem.
+# Preparando para o curso de Análise e Desenvolvimento de Sistemas.
+
+import this # The Zen of Python, by Tim Peters
+print("Olá, Alura!")
