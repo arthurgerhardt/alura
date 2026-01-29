7c14576fe273f65490d1e794cbef9b8ab93c8108
“arthurgerhardt” <renewedjesus@gmail.com>
2026-01-29 13:25:20 -0300

    Avançando no curso de Python na Alura.

---
+++ Python/Exercicios/app.py
@@ -0,0 +1,13 @@
+nome = "Arthur"
+idade = 40
+pi = 3.14159
+
+print ("Python na Escola de Programação da Alura \n")
+print (f"Meu nome é {nome} e eu tenho {idade} anos. \n")    
+print ("A")
+print ("L")
+print ("U")
+print ("R")
+print ("A")
+
+print (f"\nO valor de pi é aproximadamente {pi:.2f}")
--- Python/Projetos/sabor-express/app.py
+++ Python/Projetos/sabor-express/app.py
@@ -1,5 +1,6 @@
 # App de Sabor Express, um app para restaurantes delivery.
 # Desenvolvido durante o curso de Python da Alura.
+import os
 
 print("""
       
@@ -11,11 +12,39 @@ print("""
 ╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░
       \n""")
 
-print("1. Cadastrar restaurante")
-print("2. Listar restaurantes")
-print("3. Ativar restaurante")
-print("4. Sair \n")
+# Definição das funções do app
+def cadastrar_restaurante():
+    os.system('clear')  # Limpa a tela do terminal
+    print("Função de cadastro de restaurante.")
 
-opcao_escolhida = input("Escolha uma opção: ")
-print(f'Você escolheu a opção {opcao_escolhida}')
+def listar_restaurantes():
+    os.system('clear')  # Limpa a tela do terminal
+    print("Função de listagem de restaurantes.")
 
+def ativar_restaurante():
+    os.system('clear')  # Limpa a tela do terminal
+    print("Função de ativação de restaurante.")
+    
+def finalizar_app():
+    os.system('clear')  # Limpa a tela do terminal
+    print("Obrigado por usar o Sabor Express. Até a próxima!")
+
+# Bloco de repetição para o menu do app
+while True:
+    print("1. Cadastrar restaurante")
+    print("2. Listar restaurantes")
+    print("3. Ativar restaurante")
+    print("4. Sair \n")
+    opcao_escolhida = input("Escolha uma opção: ")
+    print(f'Você escolheu a opção {opcao_escolhida}')
+
+# Bloco condicional para tratar as opções do menu
+    if opcao_escolhida == "1":
+        cadastrar_restaurante()
+    elif opcao_escolhida == "2":
+        listar_restaurantes()
+    elif opcao_escolhida == "3":
+        ativar_restaurante()
+    else: # Tratamento da opção de saída
+        finalizar_app()
+        break
